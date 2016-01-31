fa4c29ef39978e714b5256c73b039e20556ef2bd
luisonboard <luisonboard@gmail.com>
2016-01-30 19:09:42 -0500

    Creacion de clases entidad y cambio de nombre proyecto a presentacion

---
+++ Entidades/DetalleProducto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWinProyectoo
+{
+    public class DetalleProducto
+    {
+        int num_factura;
+        int cod_producto;
+        int cantidad;
+        string descripcion;
+        double subtotal;
+
+        public int Num_factura
+        {
+            get
+            {
+                return num_factura;
+            }
+
+            set
+            {
+                num_factura = value;
+            }
+        }
+
+        public int Cod_producto
+        {
+            get
+            {
+                return cod_producto;
+            }
+
+            set
+            {
+                cod_producto = value;
+            }
+        }
+
+        public int Cantidad
+        {
+            get
+            {
+                return cantidad;
+            }
+
+            set
+            {
+                cantidad = value;
+            }
+        }
+
+        public double Subtotal
+        {
+            get
+            {
+                return subtotal;
+            }
+
+            set
+            {
+                subtotal = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                descripcion = value;
+            }
+        }
+    }
+}
---
+++ Entidades/Factura.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWinProyectoo
+{
+    public class Factura
+    {
+        int numero;
+        DateTime fecha;
+        String ced_recep;
+        String ced_cliente;
+        double subtotal;
+        double iva;
+        double total;
+
+        public int Numero
+        {
+            get
+            {
+                return numero;
+            }
+
+            set
+            {
+                numero = value;
+            }
+        }
+
+        public DateTime Fecha
+        {
+            get
+            {
+                return fecha;
+            }
+
+            set
+            {
+                fecha = value;
+            }
+        }
+
+        public string Ced_recep
+        {
+            get
+            {
+                return ced_recep;
+            }
+
+            set
+            {
+                ced_recep = value;
+            }
+        }
+
+        public string Ced_cliente
+        {
+            get
+            {
+                return ced_cliente;
+            }
+
+            set
+            {
+                ced_cliente = value;
+            }
+        }
+
+        public double Subtotal
+        {
+            get
+            {
+                return subtotal;
+            }
+
+            set
+            {
+                subtotal = value;
+            }
+        }
+
+        public double Iva
+        {
+            get
+            {
+                return iva;
+            }
+
+            set
+            {
+                iva = value;
+            }
+        }
+
+        public double Total
+        {
+            get
+            {
+                return total;
+            }
+
+            set
+            {
+                total = value;
+            }
+        }
+    }
+}
